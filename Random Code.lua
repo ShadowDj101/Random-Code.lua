a3e8a2ab09a5774b7c822994bd5b62506fc37389
ShadowDj101 <shadowdj101@yahoo.com>
2015-01-26 20:03:32 -0600

    Lotta Bit Of Code

--- Random Code.lua
+++ Random Code.lua
@@ -1,10 +1,20 @@
 --functions
-function restart()
+function code_error()
+print("Code Malfunction.")
+print("Resetting the code...  Please Wait...")
+print("Code Reset.")
+print("Type 'restart' to restart the program.")
 if io.read() == "restart" then
 print("Restarting...")
 else
+while io.read() ~= "restart" do
 print "Type 'restart' to restart the program."
 end
+end
+end
+function end_code()
+print("end")
+io.read()
 end
 --functions
 print("Hello. This is some random code I created.")
@@ -12,19 +22,17 @@ print("It might not be very interesting, but it is random.")
 print("Send suggestions to shadowdj101@yahoo.com")
 print("Thanks for the suggestions.")
 io.read()
-print("Code Malfunction.")
-print("Resetting the code...  Please Wait...")
-print("Code Reset.")
-print("Type 'restart' to restart the program.")
-while io.read() ~= "restart" do
-restart()
-end
+code_error()
 print("Program Reset.")
 print("Running...")
 print("Sprechen Sie Deutsch?")
 if io.read() == "ja" then
-print("Cool. Let's continue")
+print("Cool. So you do speak German. Let's continue.")
 else
 print("Ok. You don't speak German. Oh well.")
 end
-io.read()
+print("So, do you like the D0oW1\f_j*Uoo f$Sk9423q8><")
+if io.read() ~= "something you shouldn't type" then
+code_error()
+end
+end_code()
